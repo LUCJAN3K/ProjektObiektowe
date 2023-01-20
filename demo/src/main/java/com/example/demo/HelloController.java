6d6939d53c54ddb0f341cd1ff5d5a21a9d65ffc0
LUCJAN3K <lucm1309@gmail.com>
2023-01-20 18:41:47 +0100

    uwu
    idk

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -18,20 +18,21 @@ public class HelloController {
     private Label text;
     private Stage stage;
     private Scene scene;
-    private Parent root;
+    public Parent root;
     @FXML
     protected void exit(){
         Platform.exit();
     }
     public void SwitchSceneNewGame(ActionEvent event) throws  IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("game.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        SwitchSceneGetData(event, root);
     }
     public void SwitchSceneContinue(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("continueScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("savescreen.fxml"));
+        SwitchSceneGetData(event, root);
+    }
+
+    public void SwitchSceneGetData(ActionEvent event, Parent root) {
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
@@ -39,10 +40,6 @@ public class HelloController {
     }
     public void SwitchSceneMainMenu(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainmenu.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        SwitchSceneGetData(event, root);
     }
-
 }
